19238868aec2ded54ee1df78c3154d461d8839ea
Keyur Bhikadiya <keyur.vision2004@gmail.com>
2024-10-16 15:53:59 +0530

    db connect

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
+const connectMongoDB = require("./config/dbConnect.js")
 const express = require("express");
 const app = express();
 const router = require("./routes/index.js");
 const errorHandler = require("./errorHandling/errorMiddleware.js");
 const limiter = require("./middleware/rateLimiter.js")
 var cors = require('cors')
-const connectMongoDB = require("./config/dbConnect.js")
 
 app.use(cors({
     origin: "*"
